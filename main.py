467173708ca04a761077ccd74cd0fc8a8c6b0a50
kevin Graff <kevin@kevin-graff.com>
2023-09-28 18:35:07 -0400

    added mqtt and prams

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ MIT License
 """
 import udi_interface
 import sys
+import paho.mqtt.client as mqtt
+import json
 
 LOGGER = udi_interface.LOGGER
 
@@ -34,6 +36,14 @@ class Controller(udi_interface.Node):
         self.n_queue = []
 
         self.Parameters = Custom(polyglot, 'customparams')
+        self.mqtt_server = "localhost"
+        self.mqtt_port = 1883
+        self.mqtt_user = None
+        self.mqtt_password = None
+        self.devlist = None
+        self.INFO1 = None
+        self.setup = 0
+        self.mqtt_topic = None
 
         # subscribe to the events we want
         polyglot.subscribe(polyglot.CUSTOMPARAMS, self.parameterHandler)
@@ -42,6 +52,20 @@ class Controller(udi_interface.Node):
 
         # start processing events and create add our controller node
         polyglot.ready()
+        # start mqtt
+        polyglot.updateProfile()
+        self.mqttc = mqtt.Client()
+        self.mqttc.on_connect = self.on_connect
+        LOGGER.info("Start2")
+        self.on_disconnect = self.on_disconnect
+        self.mqttc.on_message = self.on_message
+        self.mqttc.is_connected = False
+        LOGGER.info("Start3")
+
+        self.mqttc.username_pw_set("n2uns", "kevin8386")
+        self.mqttc.connect("192.168.18.185", 1884, 60)
+        LOGGER.info("Start4")
+        self.mqttc.loop_start()
         self.poly.addNode(self)
 
     '''
@@ -50,8 +74,18 @@ class Controller(udi_interface.Node):
     '''
     def parameterHandler(self, params):
         self.Parameters.load(params)
+        self.mqtt_server = self.Parameters["mqtt_server"] or 'localhost'
+        self.mqtt_port = int(self.Parameters["mqtt_port"] or 1883)
+        if self.Parameters["mqtt_user"] is None:
+            LOGGER.error("mqtt_user must be configured")
+        if self.Parameters["mqtt_password"] is None:
+            LOGGER.error("mqtt_password must be configured")
 
-
+        self.mqtt_user = self.Parameters["mqtt_user"]
+        self.mqtt_password = self.Parameters["mqtt_password"]
+        # ***************************************    read in the topic from config
+        self.mqtt_topic = self.Parameters["mqtt_topic"]
+        LOGGER.info("prams updted")
     '''
     This is called when the node is added to the interface module. It is
     run in a separate thread.  This is only run once so you should do any
@@ -100,6 +134,32 @@ class Controller(udi_interface.Node):
         LOGGER.info('Discover not implemented')
 
     commands = {'DISCOVER': noop}
+    def on_disconnect(self, client, userdata, rc):
+        self.mqttc.is_connected = False
+        if rc != 0:
+            LOGGER.warning("Poly MQTT disconnected, trying to re-connect")
+            try:
+                self.mqttc.reconnect()
+            except Exception as ex:
+                LOGGER.error("Error connecting to Poly MQTT broker {}".format(ex))
+        else:
+            LOGGER.info("Poly MQTT graceful disconnection")
+
+    def on_connect(self, client, none, flags, rc):
+        if rc == 0:
+            LOGGER.info("Poly MQTT Connected, subscribing...")
+            self.mqttc.is_connected = True
+            result = self.mqttc.subscribe("mydevice/status")
+            if result[0] == 0:
+                LOGGER.info(
+                    "Subscribed to {} ".format("status")
+            )
+        else:
+            LOGGER.error("Poly MQTT Connect failed")
+    def on_message(client, userdata, message):
+        LOGGER.info("Received message '" + str(message.payload) + "' on topic '"
+            + message.topic + "' with QoS " + str(message.qos))
+
 
 
 
